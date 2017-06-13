3495833a8518f7fde348ac66072c9518fc827af8
Johan T. Halseth <johanth@gmail.com>
2017-06-13 18:41:58 +0200

    add android building

--- build_mobile.sh
+++ build_mobile.sh
@@ -2,13 +2,17 @@
 set -e -u -o pipefail # Fail on error
 
 dir=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
-echo "current dir is "
-echo $dir
+echo "current dir is $dir"
 cd $dir
 
 arg=${1:-}
 
 package="github.com/lightningnetwork/lnd/mobile"
+
 ios_dest="$dir/mobile/build/ios/Lndbindings.framework"
 echo "Building for iOS ($ios_dest)..."
 "$GOPATH/bin/gomobile" bind -target=ios -tags="ios" -v -o "$ios_dest" "$package"
+
+android_dest="$dir/mobile/build/android/Lndbindings.aar"
+echo "Building for Android ($android_dest)..."
+"$GOPATH/bin/gomobile" bind -target=android -tags="android" -v -o "$android_dest" "$package"
